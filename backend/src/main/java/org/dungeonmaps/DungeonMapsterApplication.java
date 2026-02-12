e101aa8844c0a9f2b932e7d84ef63f4052f3ebe0
zookski <kevin.zookski@fluxon.com>
2026-02-12 07:55:03 -0600

    Initial commit - Spring Boot backend and Angular frontend

---
+++ backend/src/main/java/org/dungeonmaps/DungeonMapsterApplication.java
@@ -0,0 +1,11 @@
+package org.dungeonmaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonMapsterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DungeonMapsterApplication.class, args);
+    }
+}
